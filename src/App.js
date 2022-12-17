bcb5053f2418c7174b82b68e8c24a1956da4e33c
tidynoob <mitchellgriffin9@gmail.com>
2022-12-17 11:29:44 -0500

    feat: set up CRA

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <div />
+    </ChakraProvider>
+  );
+}
+
+export default App;
