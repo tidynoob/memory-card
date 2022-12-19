74fd27012105ab7bf7da10c99da6a7d420a1341d
tidynoob <mitchellgriffin9@gmail.com>
2022-12-19 17:47:28 -0500

    Modify game class and update tests
    Modify Game class:
    Remove Score import and replace with a score property initialized to 0
    Add maxScore property initialized to 0
    Add shuffleCards method to shuffle the cards array
    Add selectCard method to handle selecting a card and updating the score
    Add resetScore method to reset the score to 0
    Add increaseScore method to increase the score and update maxScore if necessary
    Modify createCards method to return 'this' for method chaining
    
    Update Game.test.js:
    Add tests for shuffleCards and selectCard methods
    Update tests for score and maxScore behavior when selecting a card

--- src/features/Game.js
+++ src/features/Game.js
@@ -1,5 +1,4 @@
 import getPokemon from "./getPokemon";
-import Score from "./Score";
 import Card from "./Card";
 
 const NUM_CARDS = 10;
@@ -7,8 +6,9 @@ const NUM_CARDS = 10;
 class Game {
     constructor() {
         this.cards = [];
-        this.score = new Score();
-    }
+        this.score = 0;
+        this.maxScore = 0;
+    };
 
     createCards = async () => {
         const pokemonList = await getPokemon(NUM_CARDS);
@@ -16,7 +16,44 @@ class Game {
         // console.log(cardList);
         this.cards = cardList;
         return this;
+    };
+
+    shuffleCards = () => {
+        const cardsCopy = [...this.cards];
+        for (let i = cardsCopy.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = cardsCopy[i];
+            cardsCopy[i] = cardsCopy[j];
+            cardsCopy[j] = temp;
+        }
+        this.cards = cardsCopy;
+    };
+
+    selectCard = (cardIndex) => {
+        const selectedCard = this.cards[cardIndex];
+        if (selectedCard.selected) {
+            this.resetScore();
+            this.createCards();
+        } else {
+            selectedCard.selected = true;
+            this.increaseScore();
+            this.shuffleCards();
+        }
+    };
+
+    resetScore = () => {
+        this.score = 0;
+        return this;
+    };
+
+    increaseScore = () => {
+        this.score += 1;
+        if (this.score > this.maxScore) {
+            this.maxScore = this.score;
+        };
+        return this;
     }
+
 }
 
 export default Game;
--- src/features/Game.test.js
+++ src/features/Game.test.js
@@ -11,12 +11,30 @@ describe('testing game', () => {
     expect(testGame.cards[0].sprite).toBeTruthy();
   });
 
+  test('game can shuffle cards', async () => {
+    await testGame.createCards();
+    const firstCard = testGame.cards[0];
+    testGame.shuffleCards();
+    expect(testGame.cards[0]).not.toBe(firstCard);
+  });
+
+  test('selecting a card shuffles the cards', () => {
+    const firstCard = testGame.cards[0];
+    testGame.selectCard(0);
+    expect(testGame.cards[0]).not.toBe(firstCard);
+  });
+
   test("selecting a non-selected card increases score and max score", () => {
-    // ...
+    expect(testGame.score).toBe(1);
+    expect(testGame.score).toBe(1);
   });
 
   test("selecting a selected card resets score but keeps max score", () => {
-    // ...
+    while (testGame.score !== 0) {
+      testGame.selectCard(0);
+    }
+    expect(testGame.score).toBe(0);
+    expect(testGame.maxScore).toBeGreaterThan(0);
   });
 
 });
