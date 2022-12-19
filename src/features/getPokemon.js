ada86af6dbe95c8a527fc5bebe07da18a5713152
tidynoob <mitchellgriffin9@gmail.com>
2022-12-19 14:56:46 -0500

    Simplify getPokemon function

--- src/features/getPokemon.js
+++ src/features/getPokemon.js
@@ -1,25 +1,19 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 const AVAILABLE_SPRITES = 905;
 
 const randomNum = () => {
-    return Math.floor(Math.random() * (AVAILABLE_SPRITES)) + 1
-}
+  return Math.floor(Math.random() * AVAILABLE_SPRITES) + 1;
+};
 
-const getRandomPokemon = async () => {
-    const num = randomNum();
-    // console.log(`Fetching pokemon with id ${num}`);
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
-    const data = await response.json();
-    return data;
-}
+const getPokemon = async (id = randomNum()) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const data = await response.json();
+  return {
+    name: data.name,
+    id: data.id,
+    sprite: data.sprites.front_default,
+  };
+};
 
-const getPokemonDetails = (data) => {
-    return {
-        name: data.name,
-        id: data.id,
-        sprite: data.sprites.front_default,
-    }
-}
-
-export {getRandomPokemon,getPokemonDetails};
+export { getPokemon };
--- src/features/getPokemon.test.js
+++ src/features/getPokemon.test.js
@@ -1,25 +1,33 @@
-import {getRandomPokemon,getPokemonDetails} from './getPokemon.js'
+import {
+  getPokemon,
+} from "./getPokemon.js";
 
 describe("getPokemon", () => {
 
-    let pokemonData;
-
-    beforeAll(async () => {
-        pokemonData = await getRandomPokemon();
+  test("can fetch a random pokemon", async () => {
+    const testPokemon = await getPokemon();
+    expect(testPokemon.name).toBeTruthy();
+    expect(testPokemon.id).toBeTruthy();
+    expect(testPokemon.sprite).toBeTruthy();
+    console.log(testPokemon);
     });
 
+  test("can fetch a specific pokemon", async () => {
+    const testID = 83;
+    const testPokemon = await getPokemon(testID);
+    expect(testPokemon.name).toBeTruthy();
+    expect(testPokemon.id).toBeTruthy();
+    expect(testPokemon.sprite).toBeTruthy();
+    console.log(testPokemon);
+  });
 
-    test("can fetch a random pokemon", () => {
-
-        expect(pokemonData).toBeTruthy();
-    });
-
-    test("random pokemon has name, id, and sprite", () => {
-        const pokemon = getPokemonDetails(pokemonData);
-        expect(pokemon.name).toBeTruthy();
-        expect(pokemon.id).toBeTruthy();
-        expect(pokemon.sprite).toBeTruthy();
-        console.log(pokemon);
-    });
-
+  test("get pokemon details for an array of ids", async () => {
+    const testIDs = [1, 2, 3, 4, 5];
+    const testPokemon = await Promise.all(testIDs.map(getPokemon))
+    expect(testPokemon.length).toBe(testIDs.length);
+    expect(testPokemon[0].name).toBeTruthy();
+    expect(testPokemon[0].id).toBeTruthy();
+    expect(testPokemon[0].sprite).toBeTruthy();
+    console.log(testPokemon);
+  });
 });
