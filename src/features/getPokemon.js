bf793aaa9b686b30308eecd83ad8839271527910
tidynoob <mitchellgriffin9@gmail.com>
2022-12-19 11:42:10 -0500

    Add getPokemon.js and getPokemon.test.js to fetch pokemon data and test
    getPokemon.js includes functions to fetch a random Pokemon from the PokeAPI and get its details, including its name, ID, and sprite URL.
    getPokemon.test.js includes tests for the getRandomPokemon and getPokemonDetails functions. The tests ensure that the functions can fetch a random Pokemon and return its name, ID, and sprite URL as expected.

---
+++ src/features/getPokemon.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+
+const AVAILABLE_SPRITES = 1154;
+
+const randomNum = () => {
+    return Math.floor(Math.random() * (AVAILABLE_SPRITES)) + 1
+}
+
+const getRandomPokemon = async () => {
+    const num = randomNum();
+    console.log(`Fetching pokemon with id ${num}`);
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
+    const data = await response.json();
+    return data;
+}
+
+const getPokemonDetails = async (data) => {
+    return {
+        name: data.name,
+        id: data.id,
+        sprite: data.sprites.front_default,
+    }
+}
+
+export {getRandomPokemon,getPokemonDetails};
