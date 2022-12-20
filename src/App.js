a5d2c1f0471f6ef1395cfc55b5005c08a8f1e16c
tidynoob <mitchellgriffin9@gmail.com>
2022-12-19 19:37:43 -0500

    Add score to UI

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import MainLayout from './components/Layout/MainLayout';
+import React, { useState, useEffect } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import MainLayout from "./components/Layout/MainLayout";
 
-import Game from './features/Game';
+import Game from "./features/Game";
 
 const game = new Game();
 
 function App() {
   const [cards, setCards] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
 
   useEffect(() => {
-
     const getCards = async () => {
-      await game.createCards()
-      setCards(game.cards)
+      await game.createCards();
+      setCards(game.cards);
     };
-
     getCards();
-
   }, []);
 
   const handleClick = async (cardIndex) => {
     await game.selectCard(cardIndex);
-    const newCards = await game.cards;
+    const newCards = game.cards;
+    const newScore = game.score;
+    const newHighScore = game.maxScore;
+
     setCards(newCards);
+    setScore(newScore);
+    setHighScore(newHighScore);
   };
 
   return (
     <ChakraProvider>
-      <MainLayout cards={cards} handleClick={handleClick} />
+      <MainLayout cards={cards} handleClick={handleClick} score={score} highScore={highScore}/>
     </ChakraProvider>
   );
 }
--- src/components/Layout/MainLayout.js
+++ src/components/Layout/MainLayout.js
@@ -1,27 +1,58 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { v4 as uuidv4 } from "uuid";
 import Card from "../Elements/Card";
 
 function Cards(props) {
-    const { cards, handleClick } = props;
+  const { cards, handleClick } = props;
 
-    if (cards === undefined) {
-        return <Box>Loading...</Box>;
-    }
+  if (cards === undefined) {
+    return <Box>Loading...</Box>;
+  }
 
-    return (
-        cards.map((card, index) => <Card key={uuidv4()} data-index={index} pokemonName={card.name} sprite={card.sprite} handleClick={() => handleClick(index)} />)
-    );
+  return cards.map((card, index) => (
+    <Card
+      key={uuidv4()}
+      data-index={index}
+      pokemonName={card.name}
+      sprite={card.sprite}
+      handleClick={() => handleClick(index)}
+    />
+  ));
+}
+
+function Score(props) {
+  const { score, highScore } = props;
+  return (
+    <Box display="flex" flexDir="column" alignItems="center">
+      <Box display="grid" gridTemplateColumns="4fr 1fr" justifyItems="end" w="100%">
+        <Text as='b'>Score:</Text>
+        <Text>{score}</Text>
+      </Box>
+      <Box display="grid" gridTemplateColumns="4fr 1fr" justifyItems="end" w="100%">
+        <Text as='b'>High Score:</Text>
+        <Text>{highScore}</Text>
+      </Box>
+    </Box>
+  );
 }
 
 function MainLayout(props) {
-    const { cards, handleClick } = props;
-    return (
-        <Box display="grid" gridTemplateColumns='repeat(auto-fill, minmax(200px, 1fr))' gap="4" w="80%" mx='auto' p='4'>
-            <Cards cards={cards} handleClick={handleClick} />
-        </Box>
-    );
+  const { cards, score, highScore, handleClick } = props;
+  return (
+    <Box display="flex" flexDir="column" p="4" w="80%" alignItems="center" m="auto">
+      <Score score={score} highScore={highScore} />
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+        gap="4"
+        p="4"
+        w="100%"
+      >
+        <Cards cards={cards} handleClick={handleClick} />
+      </Box>
+    </Box>
+  );
 }
 
 export default MainLayout;
