d46469fecf2959f8e207044673fd2d587b306047
tidynoob <mitchellgriffin9@gmail.com>
2022-12-19 19:37:35 -0500

    Capitalize pokemon names

--- src/components/Elements/Card.js
+++ src/components/Elements/Card.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Box, Image, Heading } from "@chakra-ui/react";
 
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
 function Card(props) {
   const { pokemonName, sprite, handleClick, ...rest } = props;
 
@@ -19,9 +21,10 @@ function Card(props) {
       
     >
       <Image src={sprite} />
-      <Heading size="sm">{pokemonName}</Heading>
+      <Heading size="sm">{capitalize(pokemonName)}</Heading>
     </Box>
   );
 }
+
 
 export default Card;
